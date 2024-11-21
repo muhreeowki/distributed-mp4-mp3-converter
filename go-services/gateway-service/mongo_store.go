9df007bf880ea9b5bf7b8e9a78efa1c23bec2ec4
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-11-21 12:01:20 +0300

    Implemented MongoStore constructor function
    -- NewMongoStore now connects to a mongo db instance and creates a
    client and gridfs bucket and stores them in the MongoStore struct.

--- go-services/gateway-service/mongo_store.go
+++ go-services/gateway-service/mongo_store.go
@@ -1,9 +1,57 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/gridfs"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
 type Store interface{}
 
-type MongoStore struct{}
+type MongoStore struct {
+	gridfs *gridfs.Bucket
+	client *mongo.Client
+}
 
-func NewMongoStore() *MongoStore {
-	return &MongoStore{}
+func NewMongoStore(conStr string) (*MongoStore, error) {
+	// Set the server API version for the client
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(conStr).SetServerAPIOptions(serverAPI)
+
+	// Create a new client
+	client, err := mongo.Connect(context.Background(), opts)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err := client.Disconnect(context.Background()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Get the database
+	db := client.Database("admin")
+
+	// Check the connection
+	var result bson.M
+	if err := db.RunCommand(context.Background(), bson.D{{"ping", 1}}).Decode(&result); err != nil {
+		return nil, fmt.Errorf("Failed to ping MongoDB: %v", err)
+	}
+	log.Println("Successfully connected to MongoDB.")
+
+	// Create a new GridFS bucket
+	gfs, err := gridfs.NewBucket(db)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create GridFS bucket: %v", err)
+	}
+
+	return &MongoStore{
+		gridfs: gfs,
+		client: client,
+	}, nil
 }
